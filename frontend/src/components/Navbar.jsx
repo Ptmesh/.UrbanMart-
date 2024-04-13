62b0ebf7d5218b573999659edcccf619b5af2589
Prathmesh Takalkar <takalkarprathmesh@gmail.com>
2024-04-13 12:14:43 +0530

    first commit

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,88 @@
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import { Badge } from "@mui/material";
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 60px;
+`;
+
+const Wrapper = styled.div`
+  padding: 20px;
+  display: flex;
+  align-items:center
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  align-items: center;
+  display: flex;
+`;
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+
+const SearchContainer = styled.div`
+  padding: 5px;
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+`;
+
+const Input = styled.input`
+  border: none;
+  cursor: pointer;
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  margin-left: 25px;
+`;
+const Navbar = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input />
+            <SearchIcon style={{ color: "gray", fontSize: 16 }} />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>.UrbanMart!</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlinedIcon />
+            </Badge>
+          </MenuItem>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Navbar;
